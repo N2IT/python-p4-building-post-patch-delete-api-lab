6d7da4a5c32046679f8dcc8bebdf8aa67818b68e
Tony Eder <tonyeder11@gmail.com>
2024-04-18 08:12:48 -0700

    POST /baked_goods success

--- server/app.py
+++ server/app.py
@@ -30,6 +30,42 @@ def bakery_by_id(id):
     bakery_serialized = bakery.to_dict()
     return make_response ( bakery_serialized, 200  )
 
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def baked_goods():
+    if request.method == 'GET':
+        baked_goods = []
+        for baked_good in BakedGood.query.all():
+            baked_goods_dict = baked_good.to_dict()
+            baked_goods.append(baked_goods_dict)
+        
+        response = make_response(
+            baked_goods, 
+            200
+        )
+
+        return response
+
+    elif request.method == 'POST':
+        new_baked_good = BakedGood(
+            name=request.form.get("name"),
+            price=request.form.get("price"),
+            bakery_id=request.form.get("bakery_id"),
+        )
+
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        baked_goods_dict = new_baked_good.to_dict()
+        
+        response = make_response(
+            # jsonify(baked_goods_dict),  # Ensure the data is JSON-encoded
+            baked_goods_dict,
+            201
+        )
+
+        return response
+
+
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
